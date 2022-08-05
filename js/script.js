ab99d645370778bb6aa46754bda67011bdf05a8e
NitaCristian <cristiannita24@gmail.com>
2022-08-05 16:08:40 +0300

    Removed Materalize CSS from index.html page

--- js/script.js
+++ js/script.js
@@ -1,42 +1,6 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0]
+const navbarLinks = document.getElementsByClassName("navbar-links")[0]
 
-function initFAB() {
-    document.addEventListener('DOMContentLoaded', function () {
-        let elems = document.querySelectorAll('.fixed-action-btn');
-        let instances = M.FloatingActionButton.init(elems, {});
-    });
-}
-
-function initCarousel() {
-    document.addEventListener('DOMContentLoaded', function () {
-        let elems = document.querySelectorAll('.carousel');
-        let instances = M.Carousel.init(elems, {
-            fullWidth: true,
-            indicators: true
-        });
-    });
-
-}
-
-function initCollapsible() {
-    document.addEventListener('DOMContentLoaded', function () {
-        let elems = document.querySelectorAll('.collapsible');
-        let instances = M.Collapsible.init(elems, {});
-    });
-}
-
-function initNavbar() {
-    document.addEventListener('DOMContentLoaded', function () {
-        let elems = document.querySelectorAll('.sidenav');
-        let instances = M.Sidenav.init(elems, {});
-    });
-}
-
-function main() {
-    // initFAB();
-    // initCarousel();
-    // initCollapsible();
-    // initNavbar();
-    M.AutoInit();
-}
-
-main();
+toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+})
